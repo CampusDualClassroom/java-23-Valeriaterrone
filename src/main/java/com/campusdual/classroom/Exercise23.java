1a47c101568e7d8d63369432c2ad0dc4cb2992a3
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2025-01-10 14:30:52 +0100

    resuelto

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -6,15 +6,35 @@ import java.util.Map;
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
+     Map<String, Person>  map = new HashMap<>();
 
+     map.put("person", new Person("John", "Smith"));
+     map.put("teacher", new Teacher("Maria", "Montesori", "Educación"));
+     map.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+     map.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+     return map;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+    return map.put(key, value);  // añadimos nueva entrada al mapa
     }
 
     public static void printMapValues(Map<String, Person> map) {
+// recorro el mapa para imprimir los detalles de cada uno de sus valores
+        for (Map.Entry<String, Person> entry : map.entrySet()) {
+            System.out.println("Clave: " + entry.getKey());
+            entry.getValue().getDetails(); // se llama al metodo get
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        Map<String, Person> map = createHashMap(); // creamos el mapa con sus entradas iniciales
+
+        addMapValue(map, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));
+        addMapValue(map, "teacher2", new Teacher("Jane", "Perez", "Matemáticas"));
+        printMapValues(map); // imprimimos los detalles de cada valor que hay en el mapa
     }
     
 }
